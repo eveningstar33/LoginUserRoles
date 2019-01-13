0538969f5d9b59c20defa8aaa661a18b271755db
Dragos Sarpe <contact@usdgadget.com>
2019-01-13 14:11:54 +0200

    Initial import of Spring app

---
+++ src/main/java/com/dgs/springbootsecurityroles/SpringbootSecurityRolesApplication.java
@@ -0,0 +1,14 @@
+package com.dgs.springbootsecurityroles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSecurityRolesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSecurityRolesApplication.class, args);
+	}
+
+}
+
